6c48f2476a5596182d010013c44173abe98cdb02
SRI123PRIYA <144232251+SRI123PRIYA@users.noreply.github.com>
2023-09-23 21:50:05 +0530

    Add files via upload

--- unit 3 challenge 2/main (2).py
+++ unit 3 challenge 2/main (2).py
@@ -1,19 +1,39 @@
-import re
- 
-# Sample input
-arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
-x = 110
- 
-# Convert the array to a string
-arr_str = ','.join(str(i) for i in arr)
- 
-# Use regular expression to search for the element in the string
-match = re.search(r'\b{}\b'.format(x), arr_str)
- 
-# Output
-if match:
-    # Calculate the index by counting the number of commas before the match
-    result = arr_str[:match.start()].count(',')
-    print(f"Element {x} is present at index {result}")
-else:
-    print(f"Element {x} is not present in the array")
+'''
+Implement a function called sort_students that takes a list of student objects as input and sorts the
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object
+has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function
+with different input lists of students.
+'''
+
+class Student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                           key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+
+# Example usage:
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124", 8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
